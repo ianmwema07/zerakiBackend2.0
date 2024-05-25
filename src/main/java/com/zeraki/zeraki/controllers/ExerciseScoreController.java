c790509a44a8697aefe7f81a698e88818f64e66d
ian.mwema <ianmwema07@gmail.com>
2024-05-26 00:23:02 +0300

    created and secured the endpoints for exercise Scores

--- src/main/java/com/zeraki/zeraki/controllers/ExerciseScoreController.java
+++ src/main/java/com/zeraki/zeraki/controllers/ExerciseScoreController.java
@@ -1,6 +1,7 @@
 package com.zeraki.zeraki.controllers;
 
 import com.zeraki.zeraki.Entities.ExerciseScore;
+import com.zeraki.zeraki.auth.utils.JwtTokenUtil;
 import com.zeraki.zeraki.responses.CustomResponse;
 import com.zeraki.zeraki.services.ExerciseScoreService;
 import org.slf4j.Logger;
@@ -8,11 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/score")
@@ -21,36 +20,73 @@ public class ExerciseScoreController {
     @Autowired
     ExerciseScoreService exerciseScoreService;
 
+
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     //Create Exercise endpoint
     @RequestMapping(value = "/create",method = RequestMethod.POST)
-    public ResponseEntity<CustomResponse> createUser(@RequestBody ExerciseScore exerciseScore){
+    public ResponseEntity<CustomResponse> createUser(@RequestHeader(value = "Authorization", required = false)String token,@RequestBody ExerciseScore exerciseScore){
         logger.info("create ExerciseScore request :: " + exerciseScore.toString());
+        if (token == null) {
+            logger.error("The token is empty :: ");
+            CustomResponse customResponse = new CustomResponse("Token is required to proceed",HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<CustomResponse>(customResponse, HttpStatus.UNAUTHORIZED);
+        } else {
+            String realToken = token.substring(7);
+            String tokenCheckResult = jwtTokenUtil.validateToken(realToken);
+            if (tokenCheckResult.equalsIgnoreCase("valid")) {
 
         //Check function goes here
         try{
             ExerciseScore exerciseScore1 = exerciseScoreService.createExerciseScore(exerciseScore);
-            CustomResponse customResponse = new CustomResponse("Exercise Created Successfully",exerciseScore1);
+            CustomResponse customResponse = new CustomResponse("Exercise Created Successfully","CREATED SUCCESSFULLY");
             return new ResponseEntity<CustomResponse> (customResponse, HttpStatus.CREATED);
         }catch (Exception exception){
             logger.error("Exception has occurred :: " + exception.getMessage());
             CustomResponse errorResponse = new CustomResponse(exception.getMessage(),"An error has occured");
             return new ResponseEntity<CustomResponse>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
         }
+            }
+            else {
+                CustomResponse errorResponse = new CustomResponse(tokenCheckResult, "Unauthorized request ");
+                return new ResponseEntity<CustomResponse>(errorResponse, HttpStatus.UNAUTHORIZED);
+            }
+        }
     }
 
     //update Exercise
     @RequestMapping(value = "/update/{id}",method = RequestMethod.PUT)
-    public ResponseEntity<CustomResponse> updateUser(@PathVariable Long id, @RequestBody ExerciseScore exerciseScore){
+    public ResponseEntity<CustomResponse> updateUser(@RequestHeader(value = "Authorization", required = false)String token, @PathVariable Long id, @RequestBody ExerciseScore exerciseScore){
         logger.info("Exercise Score update request :: " + exerciseScore.toString());
-        try{
-            ExerciseScore exerciseScore1 = exerciseScoreService.upDateExerciseScore(id, exerciseScore);
-            CustomResponse customResponse = new CustomResponse("Exercise updated Successfully",exerciseScore1);
-            return new ResponseEntity<CustomResponse> (customResponse, HttpStatus.OK);
-        }catch (Exception exception){
-            logger.error("Exception has occurred :: " + exception.getMessage());
-            CustomResponse errorResponse = new CustomResponse(exception.getMessage(),"An error has occured");
-            return new ResponseEntity<CustomResponse>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        if (token == null) {
+            logger.error("The token is empty :: ");
+            CustomResponse customResponse = new CustomResponse("Token is required to proceed",HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<CustomResponse>(customResponse, HttpStatus.UNAUTHORIZED);
+        } else {
+            String realToken = token.substring(7);
+            String tokenCheckResult = jwtTokenUtil.validateToken(realToken);
+            if (tokenCheckResult.equalsIgnoreCase("valid")) {
+                try {
+                    ExerciseScore exerciseScore1 = exerciseScoreService.upDateExerciseScore(id, exerciseScore);
+                    CustomResponse customResponse = new CustomResponse("Exercise updated Successfully", "UPDATED SUCCESSFULLY");
+                    return new ResponseEntity<CustomResponse>(customResponse, HttpStatus.OK);
+                } catch (Exception exception) {
+                    logger.error("Exception has occurred :: " + exception.getMessage());
+                    CustomResponse errorResponse = new CustomResponse(exception.getMessage(), "An error has occured");
+                    return new ResponseEntity<CustomResponse>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            }
+            else {
+                    CustomResponse errorResponse = new CustomResponse(tokenCheckResult, "Unauthorized request ");
+                    return new ResponseEntity<CustomResponse>(errorResponse, HttpStatus.UNAUTHORIZED);
+                }
+            }
+
     }
 
 }
